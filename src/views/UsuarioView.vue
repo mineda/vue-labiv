6293b24155841db75de246d32089c67dd2e09b5c
Emanuel Mineda Carneiro <mineda@gmail.com>
2022-09-07 00:07:28 +0000

    Tela de usuário

---
+++ src/views/UsuarioView.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="usuario">
+        <p v-if="nome.length > 5">{{nome}}</p>
+        <p><input type="text" v-model="nome"><button @click="inserirUsuario()">Ok</button></p>
+        <ul>
+            <li v-for="(usuario, i) in usuarios" :key="i">{{usuario}}</li>
+        </ul>
+    </div>    
+</template>
+
+<script>
+export default {
+    name: 'usuarioView',
+    data() {
+        return {
+            nome: 'Mineda',
+            usuarios: ['oi', 'tchau']
+        }
+    },
+    methods: {
+        inserirUsuario() {
+            this.usuarios.push(this.nome);
+            this.nome = '';
+        }
+    }
+}
+</script>
